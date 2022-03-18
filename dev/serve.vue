bbdfed9097de97c72a0a9a35e135dbb3cb1da6c9
cargovova <cargovova@gmail.com>
2022-03-18 15:27:59 +0400

    feat: added a month without heatmapping.

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <vue-hm-calendar firstWeekDay="MONDAY" :monthNumber="2" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueHmCalendar from '@/vue-hm-calendar.vue'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueHmCalendar,
+  },
+})
+</script>
