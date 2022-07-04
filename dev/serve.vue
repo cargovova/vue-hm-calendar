618cb5d776ba1c08c3d7dba03e0f9e8dbd3b8527
cargovova <cargovova@gmail.com>
2022-07-04 17:01:21 +0400

    updated app version

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="app">
-    <vue-hm-calendar />
+    <br />
+    <br />
+    <vue-hm-calendar
+      mode="year"
+      :yearNumber="2022"
+      cellSize="15px"
+      firstWeekDay="monday"
+      :hideWeekNames="false"
+      :hideHeader="false"
+      :eventsDays="{
+        '2022-12-31': 10,
+        '2022-01-01': 2,
+        '2022-03-23': 1,
+        '2022-03-24': 1,
+        '2022-03-25': 2,
+        '2022-03-26': 1000,
+        '2022-01-05': 2,
+        '2022-08-05': 1,
+        '2022-08-06': 2,
+        '2022-08-07': 3,
+      }"
+      :futureEventsColors="['#42A5F5', '#1976D2', '#0D47A1']"
+    />
   </div>
 </template>
 
